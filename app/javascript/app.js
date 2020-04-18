3d1f559415cf91365b815e1ad2ccef54ca8dac6e
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-04-18 14:31:00 -0300

    Incluindo o Bulma

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
+import 'app.scss'
 
 const App = _ => {
   return(
     <Fragment>
-      Hello App
+      <Button color='primary'>
+        Testando o bulma
+      </Button>
     </Fragment>
   )
 }
